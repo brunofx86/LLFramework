922eabd24fa65ceab0a90ea6532574c6cc095862
brunofx86 <brunofx86@gmail.com>
2022-09-08 14:57:40 -0300

    monadic system

---
+++ SL/Monadic/OSTactics.v
@@ -0,0 +1,178 @@
+Require Export LL.SL.Dyadic.StructuralRules.
+
+Export LLNotations.
+Set Implicit Arguments.
+
+(* Instance LL2N_morphism {OLS : OLSig} n:
+  Proper (Permutation (A:=oo) ==> Permutation (A:=oo) ==> iff) (LL2N n).
+Proof.
+  unfold Proper; unfold respectful. 
+  intros B1 B2 PB L1 L2 PL.
+  split; intro H.
+  refine (LL2N_compat PB PL H).
+  refine (LL2N_compat (symmetry PB) (symmetry PL) H).
+Qed.
+
+ 
+ Instance LL2S_morphism {OLS : OLSig}:
+  Proper (Permutation (A:=oo) ==> Permutation (A:=oo) ==> iff) (LL2S).
+Proof.
+  unfold Proper; unfold respectful. 
+  intros B1 B2 PB L1 L2 PL.
+  split; intro H.
+  refine (LL2S_compat PB PL H).
+  refine (LL2S_compat (symmetry PB) (symmetry PL) H).
+Qed.
+ *)  
+
+Ltac solvell2 :=
+ try
+ match goal with
+ | [ H: LL2N _ ?B ?L |- LL2S ?B ?L] => apply LL2NtoLL2S in H;auto
+ end. 
+   
+Ltac llExact H :=
+  let G:= type of H in
+  match G with
+  | (LL2N ?x ?Gamma ?Delta) =>
+    match goal with
+    | [ |- LL2N ?x ?Gamma' ?Delta'] =>
+      eapply @LL2N_compat with (B1:=Gamma) (L1:=Delta); try perm
+    end 
+  end;auto.
+ 
+Ltac llExact' H :=
+  let G:= type of H in
+  match G with
+  | (LL2S ?Gamma ?Delta) =>
+    match goal with
+    | [ |- LL2S ?Gamma' ?Delta'] =>
+      eapply @LL2S_compat with (B1:=Gamma) (L1:=Delta); try perm
+    end 
+  end;auto.
+
+
+Ltac LLExact H := 
+  match (type of H) with
+  | LL2S _ _  =>  llExact' H
+  | LL2N _ _ _ => llExact H
+  end.
+
+  Definition CutW {OL:OLSig} (w: nat) :=  
+    forall m i j C A M N B, 
+    m <= w ->
+    complexity C = m ->
+      (LL2 i |-- B; C::M -> LL2 j |-- B; (dual C)::N -> LL2 |-- B; M ++ N) /\
+      (S (complexity A) = complexity C ->
+       LL2 i |-- A::B ; M -> LL2 j |-- B; (! dual A)::N -> LL2 |-- B; M++N). 
+
+  Definition CutH {OL:OLSig} (w h: nat) :=  
+    forall m i j C A M N B, 
+    m <= h ->
+    m = i + j ->
+    complexity C = S w ->
+      (LL2 i |-- B; C::M -> LL2 j |-- B; (dual C)::N -> LL2 |-- B; M ++ N) /\
+      (S (complexity A) = complexity C ->
+      LL2 i |-- A::B ; M -> LL2 j |-- B; (! dual A)::N -> LL2 |-- B; M++N). 
+
+
+Ltac dualSimpl :=
+ match goal with
+ | H: ?F = ?C^ |- _ => 
+    apply dualSubst in H;subst
+ | H: ?C^ = ?F |- _ => 
+   symmetry in H; apply dualSubst in H;subst    
+ | H: context [((?C)^)^] |- _ => 
+    rewrite <- ng_involutive in H  
+end;auto.    
+
+
+Ltac putFirst H TT :=
+match type of H with
+| LL2N ?x ?BB (?FF::TT::?XX) => 
+    eapply LL2N_compat with 
+     (B2:=BB) (L2:= TT::FF::XX) in H;try perm
+ | LL2N ?x ?BB (?FF::?GG::TT::?XX) => 
+   eapply LL2N_compat with 
+     (B2:=BB) (L2:= TT::FF::GG::XX) in H;try perm
+end.
+
+ Ltac applyCutH := 
+  match goal with
+  | [ H: CutH _ _ |- LL2N ?x (?FF::?BX) _ -> 
+         LL2N ?y ?BX _ -> 
+         LL2S _ _ ] => eapply H with (m := x + y) (C:=Quest FF);sauto
+  | [ H: CutH _ _ |- LL2N ?x _ _ -> 
+         LL2N ?y _ _ -> 
+         LL2S _ _ ] => eapply H with (m := x + y);sauto
+  | _ => idtac end.
+
+
+Ltac applyCutW := 
+  match goal with
+ | [ H: CutW _ |- LL2N ?x (?FF::?BX) _ -> 
+         LL2N ?y ?BX _ -> 
+         LL2S _ _ ] => eapply @H with (m := x + y) (C:=Quest FF);sauto
+ 
+  | [ H: CutW _ |- LL2N _ _ (?CF::_) -> 
+         LL2N _ _ _ -> 
+         LL2S _ _ ] => eapply H with (m := complexity CF);sauto
+  | _ => idtac end.
+  
+Tactic Notation "cutH" constr(P1) constr(P2) :=
+   let tP1 := type of P1 in
+   let H' := fresh "HCUT" in
+   match tP1 with
+   | LL2N ?x ?BX (?CF::?CX) => let tP2 := type of P2 in
+                    match tP2 with 
+                    | LL2N ?y ?BX (_::?CY) =>  
+                           assert(H': tP1 -> tP2 -> LL2S BX (CX++CY));
+                           applyCutH; try rewrite app_nil_r in H' 
+                    | _ => idtac "type of " P2 " is " tP2 end
+   | LL2N ?x ?BX (?NF::?CF::?CX2) => let tP2 := type of P2 in
+                    match tP2 with 
+                    | LL2N ?y ?BX (_::?CY) =>  
+                           assert(LL2N x BX (CF::(NF++CX2)) -> tP2 -> LL2S BX ((NF::CX2)++CY));
+                           applyCutH; try rewrite app_nil_r in H' 
+                    | _ => idtac "type of " P2 " is " tP2 end
+
+   | _ => idtac "type of " P1 " is " tP1 end.
+  
+Tactic Notation "cutW" constr(P1) constr(P2) :=
+   let tP1 := type of P1 in
+   let H' := fresh "WCUT" in
+   match tP1 with
+   | LL2N ?x ?BX (?CF::?CX) => let tP2 := type of P2 in
+                    match tP2 with 
+                    | LL2N ?y ?BX (_::?CY) =>  
+                           assert(H': tP1 -> tP2 -> LL2S BX (CX++CY));
+                           applyCutW; try rewrite app_nil_r in H'
+                    | _ => idtac "type of " P2 " is " tP2 end
+   | _ => idtac "type of " P1 " is " tP1 end.
+
+Tactic Notation "cutH'" constr(P1) constr(P2) :=
+   let tP1 := type of P1 in
+   let H' := fresh "HCUT" in
+   match tP1 with
+   | LL2N ?x (?CF::?BX) (?CX) => let tP2 := type of P2 in
+                    match tP2 with 
+                    | LL2N ?y ?BX (_::?CY) =>  
+                           assert(H': tP1 -> tP2 -> LL2S BX (CX++CY));
+                           applyCutH; try rewrite app_nil_r in H'
+                    | _ => idtac "type of " P2 " is " tP2 end
+   | _ => idtac "type of " P1 " is " tP1 end.
+  
+Tactic Notation "cutW'" constr(P1) constr(P2) :=
+   let tP1 := type of P1 in
+   let H' := fresh "WCUT" in
+   match tP1 with
+   | LL2N ?x (?CF::?BX) ?CX => let tP2 := type of P2 in
+                    match tP2 with 
+                    | LL2N ?y ?BX (_::?CY) =>  
+                           assert(H': tP1 -> tP2 -> LL2S BX (CX++CY));
+                           applyCutW; try rewrite app_nil_r in H'
+                    | _ => idtac "type of " P2 " is " tP2 end
+   | _ => idtac "type of " P1 " is " tP1 end.    
+
+
+     
---
+++ SL/Monadic/Sequent.v
@@ -0,0 +1,107 @@
+Require Export LL.Misc.Utils. 
+Require Export LL.SL.Syntax.
+
+Export ListNotations.
+Set Implicit Arguments.
+
+Section LL1Sequent.
+  Context `{OLS: OLSig}.
+  Definition multiset := list.
+ 
+  Reserved Notation "n '|--'  L " (at level 80).
+
+  Inductive LL1N:  nat -> multiset oo -> Prop :=
+  | ll1_init : forall A L n, Permutation L [atom A; perp A] -> n |--  L
+  | ll1_one : forall n, n |-- [One]
+  
+  | ll1_tensor : forall M N F G L n, Permutation L ((MAnd F G)::(M ++ N)) ->
+                                        (n |-- F::M) ->
+                                        (n |-- G::N) ->
+                                        (S n) |-- L 
+  | ll1_plus1 : forall M F G L n, Permutation L ((AOr F G)::M) ->
+      n |-- F::M -> S n |--  L
+  | ll1_plus2 : forall M F G L n, Permutation L ((AOr F G)::M) ->
+      n |--  G::M -> S n |--  L
+      
+  | ll1_bang : forall M L F n, Permutation L ((Bang F) :: map Quest M)->
+      n |--  F:: map Quest M -> S n |-- L
+  
+  | ll1_top : forall M L n, Permutation L (Top :: M) ->
+      n |--  L
+  | ll1_bot : forall M L n, Permutation L (Bot :: M) ->
+      n |--  M -> S n |--  L
+  | ll1_par : forall M F G L n, Permutation L ((MOr F G) :: M) ->
+      n |--  F::G::M -> S n |--  L
+  | ll1_with : forall M F G L n, Permutation L ((AAnd F G)::M) ->
+      n |--  F :: M ->
+      n |--  G :: M -> S n |--  L
+  | ll1_quest : forall M F L n, Permutation L ((Quest F) :: M) ->
+      n |-- F:: M -> S n |--  L
+  
+  | ll1_ex  : forall FX M L t n,  Permutation L ((Some  FX)::M) ->
+      uniform_oo FX -> proper t -> n |-- (FX t)::M -> S n|--  L
+  | ll1_fx  : forall FX M L n, Permutation L ((All  FX)::M) ->
+      uniform_oo FX -> (forall x, proper x -> n |--  (FX x) ::  M) ->
+      S n |--  L                                                      
+  | ll1_weak : forall M F L n, Permutation L ((Quest F) :: M) ->
+      n |-- M -> S n |--  L
+  | ll1_contr : forall M F L n, Permutation L ((Quest F) :: M) ->
+      n |-- (Quest F) :: L -> S n |--  L
+      
+                                                                          
+  where "n '|--' L " := (LL1N n L).
+   
+  Reserved Notation "'|--' L" (at level 80).
+
+  Inductive LL1S: multiset oo -> Prop :=
+  | ll1_init' : forall A L, Permutation L [atom A; perp A] -> |--  L
+  | ll1_one' :  |--  [One]
+  
+  | ll1_tensor' : forall M N F G L, Permutation L ((MAnd F G)::(M ++ N)) ->
+                                        |--  F::M ->
+                                        |--  G::N ->
+                                        |--  L 
+  | ll1_plus1' : forall  M F G L, Permutation L ((AOr F G)::M) ->
+      |--  F::M -> |--  L
+  | ll1_plus2' : forall  M F G L, Permutation L ((AOr F G)::M) ->
+      |--  G::M -> |--  L
+
+   | ll1_bang' : forall M L F, Permutation L ((Bang F) :: map Quest M)->
+      |--  F::map Quest M -> |-- L
+
+  
+  | ll1_top' : forall M L, Permutation L (Top :: M) ->
+      |--  L
+  | ll1_bot' : forall M L, Permutation L (Bot :: M) ->
+      |--  M -> |--  L
+  | ll1_par' : forall M F G L, Permutation L ((MOr F G) :: M) ->
+      |--  F::G::M -> |--  L
+  | ll1_with' : forall M F G L, Permutation L ((AAnd F G)::M) ->
+      |--  F :: M ->
+      |--  G :: M -> |--  L
+  | ll1_quest' : forall M F L, Permutation L ((Quest F) :: M) ->
+      |-- F:: M -> |--  L
+ 
+   | ll1_ex'  : forall FX M L t,  Permutation L ((Some  FX)::M) ->
+      uniform_oo FX -> proper t -> |-- (FX t)::M -> |--  L
+  | ll1_fx'  : forall FX M L, Permutation L ((All  FX)::M) ->
+      uniform_oo FX -> (forall x, proper x -> |--  (FX x) ::  M) ->
+      |--  L                                              
+  | ll1_weak' : forall M F L, Permutation L ((Quest F) :: M) ->
+       |-- M -> |--  L
+  | ll1_contr' : forall M F L, Permutation L ((Quest F) :: M) ->
+      |-- (Quest F) :: L -> |--  L
+                                                                              
+  where "'|--'  L " := (LL1S L).
+
+  
+ End LL1Sequent .
+
+Global Hint Constructors LL1N : core .
+Global Hint Constructors LL1S : core. 
+ 
+
+
+Notation "'LL1' n '|--' L " := (LL1N n L)  (at level 80).
+Notation "'LL1' '|--' L " := (LL1S L)  (at level 80).
+
